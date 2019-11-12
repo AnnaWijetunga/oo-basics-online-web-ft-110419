855c84d23945aa076806fb88dee2dd7f44d256a2
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-12 15:16:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe 
+end
+
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title 
+   
+#   def initialize(title)
+#     @title = title
+#   end
+  
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end 
+
+# book = Book.new("Some Title")
+# book.turn_page
+
+# end
